d3da0dc3f382a0563b98cdc5329e79b314e1d95f
genie <mgenie93@gmail.com>
2023-10-24 13:22:45 +0900

    mei

--- src/main/java/com/care/am/controller/commonController.java
+++ src/main/java/com/care/am/controller/commonController.java
@@ -1,14 +1,25 @@
 package com.care.am.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.care.am.common.LoginSession;
+import com.care.am.service.customer.customerService;
+import com.care.am.service.medi.mediService;
+
 @Controller
 public class commonController {
+	
+	@Autowired customerService cs;
+	@Autowired mediService ms;
 	
 	@GetMapping("/") //메인 홈페이지
 	public String main() {
@@ -26,15 +37,19 @@ public class commonController {
 	}
 	
 	@RequestMapping("logout") //로그아웃
-	public String logout(HttpSession session) {
+	public String logout(HttpSession session, 
+						@CookieValue(value="loginCookie",required=false)Cookie cookie,
+						HttpServletResponse res){
+		if(cookie != null) {
+			cookie.setMaxAge(0);
+			cookie.setPath("/");
+			res.addCookie(cookie);
+			cs.keepLogin("nan", 
+					(String)session.getAttribute(LoginSession.LOGIN));
+			System.out.println("로그아웃완료인뎅");
+		}
 		session.invalidate();
-		return "am/common/main";
+		return "redirect:/";
 	}
-	
-	@GetMapping("reservationPage")
-	public String reservationPage() {
-		return "am/reservation/reservationPage";
-	}
-	
 
 }
