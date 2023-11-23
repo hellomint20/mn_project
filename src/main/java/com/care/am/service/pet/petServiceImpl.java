b43e9a8b6506a73551e4623e65040ab171e072e4
jaeyeon <chlwodus1202@naver.com>
2023-11-23 11:42:18 +0900

    C:/Program Files/Git/

--- src/main/java/com/care/am/service/pet/petServiceImpl.java
+++ src/main/java/com/care/am/service/pet/petServiceImpl.java
@@ -77,6 +77,8 @@ public class petServiceImpl implements petService {
 		
 		if(originName == "") {
 			dto.setpPhoto(dbImg);
+
+			System.out.println("디비에 있던 원래 파일 저장===="+dbImg);
 		}else {
 			dto.setpPhoto(pfs.saveFile(file));
 			if(!dbImg.equals("petDefault.jpg")) {
